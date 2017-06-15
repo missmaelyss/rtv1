bd4242352fdc4bedd8ab2383f27a977fad3c8fa3
ele-cren <ele-cren@student.42.fr>
2017-06-15 11:27:18 +0200

    opti thread

--- propre/src/thread.c
+++ propre/src/thread.c
@@ -6,7 +6,7 @@
 /*   By: ghubert <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/14 17:58:47 by ghubert           #+#    #+#             */
-/*   Updated: 2017/06/15 10:55:19 by ghubert          ###   ########.fr       */
+/*   Updated: 2017/06/15 11:26:06 by ele-cren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ int		thread_1(void *env_1)
 
 	env = (t_env *)env_1;
 	ft_init_start(env);
-	env->sdl.pos.y = 0;
 	SDL_LockTexture(env->sdl.draw, NULL, &env->sdl.tmp, &env->sdl.pitch);
 	env->sdl.pixels = env->sdl.tmp;
+	env->sdl.pos.y = 0;
 	while (env->sdl.pos.y < (HEIGHT / 2))
 	{
 		env->sdl.pos.x = 0;
@@ -28,7 +28,7 @@ int		thread_1(void *env_1)
 		{
 			ft_init_pixel(env);
 			ft_browse_list(env);
-			if (env->calc.solution >= 0)
+			if (env->tmp.solution >= 0)
 			{
 				env->sdl.pixels[env->sdl.pos.x + (env->sdl.pos.y * WIDTH)] = \
 				SDL_MapRGBA(env->sdl.format, env->tmp.current->color.red, \
@@ -39,7 +39,6 @@ int		thread_1(void *env_1)
 		}
 		env->sdl.pos.y++;
 	}
-	SDL_UnlockTexture(env->sdl.draw);
 	return (0);
 }
 
@@ -50,8 +49,6 @@ int		thread_2(void *env_1)
 	env = (t_env *)env_1;
 	ft_init_start(env);
 	env->sdl.pos.y = 0;
-	SDL_LockTexture(env->sdl.draw, NULL, &env->sdl.tmp, &env->sdl.pitch);
-	env->sdl.pixels = env->sdl.tmp;
 	while (env->sdl.pos.y < (HEIGHT / 2))
 	{
 		env->sdl.pos.x = WIDTH / 2;
@@ -59,7 +56,7 @@ int		thread_2(void *env_1)
 		{
 			ft_init_pixel(env);
 			ft_browse_list(env);
-			if (env->calc.solution >= 0)
+			if (env->tmp.solution >= 0)
 			{
 				env->sdl.pixels[env->sdl.pos.x + (env->sdl.pos.y * WIDTH)] = \
 				SDL_MapRGBA(env->sdl.format, env->tmp.current->color.red, \
@@ -70,7 +67,6 @@ int		thread_2(void *env_1)
 		}
 		env->sdl.pos.y++;
 	}
-	SDL_UnlockTexture(env->sdl.draw);
 	return (0);
 }
 
@@ -81,8 +77,6 @@ int		thread_3(void *env_1)
 	env = (t_env *)env_1;
 	ft_init_start(env);
 	env->sdl.pos.y = HEIGHT / 2;
-	SDL_LockTexture(env->sdl.draw, NULL, &env->sdl.tmp, &env->sdl.pitch);
-	env->sdl.pixels = env->sdl.tmp;
 	while (env->sdl.pos.y < HEIGHT)
 	{
 		env->sdl.pos.x = 0;
@@ -90,7 +84,7 @@ int		thread_3(void *env_1)
 		{
 			ft_init_pixel(env);
 			ft_browse_list(env);
-			if (env->calc.solution >= 0)
+			if (env->tmp.solution >= 0)
 			{
 				env->sdl.pixels[env->sdl.pos.x + (env->sdl.pos.y * WIDTH)] = \
 				SDL_MapRGBA(env->sdl.format, env->tmp.current->color.red, \
@@ -101,7 +95,6 @@ int		thread_3(void *env_1)
 		}
 		env->sdl.pos.y++;
 	}
-	SDL_UnlockTexture(env->sdl.draw);
 	return (0);
 }
 
@@ -112,8 +105,6 @@ int		thread_4(void *env_1)
 	env = (t_env *)env_1;
 	ft_init_start(env);
 	env->sdl.pos.y = HEIGHT / 2;
-	SDL_LockTexture(env->sdl.draw, NULL, &env->sdl.tmp, &env->sdl.pitch);
-	env->sdl.pixels = env->sdl.tmp;
 	while (env->sdl.pos.y < HEIGHT)
 	{
 		env->sdl.pos.x = WIDTH / 2;
@@ -121,7 +112,7 @@ int		thread_4(void *env_1)
 		{
 			ft_init_pixel(env);
 			ft_browse_list(env);
-			if (env->calc.solution >= 0)
+			if (env->tmp.solution >= 0)
 			{
 				env->sdl.pixels[env->sdl.pos.x + (env->sdl.pos.y * WIDTH)] = \
 				SDL_MapRGBA(env->sdl.format, env->tmp.current->color.red, \
@@ -146,13 +137,13 @@ void	ft_thread(t_env *env)
 	env->thread.t_3 = NULL;
 	env->thread.t_4 = NULL;
 	env->thread.t_1 = SDL_CreateThread(thread_1, "T1", (void *)env);
-	SDL_Delay(175);
+	SDL_Delay(80);
 	env->thread.t_2 = SDL_CreateThread(thread_2, "T2", (void *)env);
-	SDL_Delay(175);
+	SDL_Delay(80);
 	env->thread.t_3 = SDL_CreateThread(thread_3, "T3", (void *)env);
-	SDL_Delay(175);
+	SDL_Delay(80);
 	env->thread.t_4 = SDL_CreateThread(thread_4, "T4", (void *)env);
-	SDL_Delay(175);
+//	SDL_Delay(170);
 	SDL_WaitThread(env->thread.t_1, &ret);
 	SDL_WaitThread(env->thread.t_2, &ret);
 	SDL_WaitThread(env->thread.t_3, &ret);
